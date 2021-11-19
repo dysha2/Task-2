83bdd46eafa27bfa3f05f6b9a7f170911343c80a
dyha035@gmail.com <dyha035@gmail.com>
2021-11-19 17:50:24 +0500

    Добавьте файлы проекта.

---
+++ Task 2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+
+namespace Task_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] data = File.ReadAllLines("input.txt");
+            int MaxStrings = int.Parse(data[0].Split(" ")[0]);
+            int Paragraph = int.Parse(data[0].Split(" ")[1]);
+            int Pages = 0;
+            int FreeLines = MaxStrings;
+            foreach(string n in data[1].Split(" "))
+            {
+                if (FreeLines - int.Parse(n) < 0)
+                {
+                    FreeLines = MaxStrings - int.Parse(n);
+                    ++Pages;
+                }
+                else if (FreeLines - int.Parse(n) == 0)
+                {
+                    FreeLines = MaxStrings;
+                    ++Pages;
+                }
+                else
+                {
+                    FreeLines -= int.Parse(n);
+                }
+            }
+            if (FreeLines < 10) ++Pages;
+            File.WriteAllText("output.txt", Pages.ToString());
+        }
+    }
+
+}
